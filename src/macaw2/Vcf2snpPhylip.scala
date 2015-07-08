7fee7594ca4950bc956766ffe96c2d713b0265e6
Arlin <arlinkeo@gmail.com>
2015-07-08 15:12:29 +0200

    Removed option to count fraction of Ns in each sequence. Instead use another script to do this.

--- src/macaw2/Vcf2snpPhylip.scala
+++ src/macaw2/Vcf2snpPhylip.scala
@@ -39,7 +39,7 @@ object Vcf2snpPhylip {
       case SNP(p, r, a) => true
       case _ => false
     }
-    
+
     def invalidSite(line: String): Boolean = {
       val arr = line.split("\t")
       if (arr(4) == "." && arr(6) == "PASS") true
@@ -62,7 +62,7 @@ object Vcf2snpPhylip {
       val writer = new PrintWriter(f)
       try { op(writer) } finally { writer.close() }
     }
-    
+
     /** Return new name of samples with 9 characters and a space. */
     def truncateName(s: String): String = {
       if (s.length > 10) s.substring(s.length - 9, s.length) + " " //Cut s to length 10
@@ -74,8 +74,6 @@ object Vcf2snpPhylip {
       case 2 => time {
         val fileList = Source.fromFile(new File(args(0))).getLines.map(new File(_)).toList
         val refMap = getPositions(fileList) // Map with ref. positions and bases
-        var excludesCount = 0
-        var excludeList: List[(String, Int)] = List()
         println("Writing phy-file...")
         printToFile(new File(args(1))) { p =>
           p.println(fileList.size + 1 + " " + refMap.size) //Print total number of sequences (VCF's) + reference (1st sequence) and total number of SNP positions.
@@ -83,30 +81,21 @@ object Vcf2snpPhylip {
           refMap.keysIterator.toList.sorted.foreach(pos => p.print(refMap(pos)))
           fileList.foreach { file => // for each file print sequence
             val name = file.getParentFile.getName
-            val snpMap = Source.fromFile(file).getLines.filterNot(_.startsWith("#")).filter(isSNP(_)).map( _ match {
-              case SNP(p, r, a) => (p, a) 
+            val snpMap = Source.fromFile(file).getLines.filterNot(_.startsWith("#")).filter(isSNP(_)).map(_ match {
+              case SNP(p, r, a) => (p, a)
             }).toMap
-            val nonSnpSet = (Source.fromFile(file).getLines.filterNot(_.startsWith("#")).filterNot(isSNP(_)).filter(line => 
+            val nonSnpSet = (Source.fromFile(file).getLines.filterNot(_.startsWith("#")).filterNot(isSNP(_)).filter(line =>
               refMap.contains(line.split("\t")(1).toInt)).map(line => line.split("\t")(1).toInt)).toSet
-            val snpSeq = refMap.keysIterator.toList.sorted.map(pos => 
+            val snpSeq = refMap.keysIterator.toList.sorted.map(pos =>
               if (snpMap.contains(pos)) snpMap(pos)
               else if (nonSnpSet.contains(pos)) "N"
-              else refMap(pos)
-            ).mkString
+              else refMap(pos)).mkString
             p.println
             p.print(truncateName(name) + snpSeq)
             println(name + ":\t" + snpMap.size + "\tSNPs")
-            val ncount = snpSeq.filter(_ == 'N').size
-            if (ncount.toFloat / refMap.size > 0.05) { //If number of N in sequence is more than 5% of total sequence.
-              excludesCount = excludesCount + 1    
-              excludeList = (name, ncount) :: excludeList
-            }
           }
         }
         println("Total of " + fileList.size + " VCFs read.")
-        println("WARNING: there are " + excludesCount + " sequences with more than 5% Ns.")
-        println("Consider rerunning this tool without these samples:")
-        excludeList.reverse.foreach(x => println(x._1 + " has " + x._2 + "Ns."))
         println("Output: " + args(1))
       }
       case _ => println(usage)
