df7d569d351b22cb00948aaa9aa29e07948d45e8
Arlin <arlinkeo@gmail.com>
2015-11-18 12:34:17 +0100

    Spacing of instructions

--- src/ara/Console.scala
+++ src/ara/Console.scala
@@ -40,7 +40,7 @@ object Console {
       println
       println("\tvcf2snp-phylip\t\tWrite phy-file from SNP sequences.")
       //println("\tmutation-rate-DR\tCount mutations within 21 bp window of DR markers given of list of VCFs.")
-      println("\tqc-snp-sequences\t\tCheck snp-sequences (phy-file) with >0.05 unknown bases N.")
+      println("\tqc-snp-sequences\tCheck snp-sequences (phy-file) with >0.05 unknown bases N.")
       println("\tqc-snp-fasta\t\tCheck length or N-content of multi fasta-file with SNP sequences.")
       //println("\tdr-markers\t\tCreate 21bp markers from a list with known TB drug resistance mutations.")
       println("\thier-clusters\t\tHierarchical clusters of tree, given lineage information.")
@@ -48,7 +48,7 @@ object Console {
       println("\tall-bj-markers\t\tCombine multiple marker files into one.")
       println("\tref-DR-region\t\tCreate minimized version of reference genome containing known DR-regions.")
       println
-      println("\tprepare-drmapping\t\tPrepare slurm script for each sample to detect drug resistance.")
+      println("\tprepare-drmapping\tPrepare slurm script for each sample to detect drug resistance.")
       println("\tprepare-bluejay\t\tPrepare slurm script for hierarchical clusters for SNP-association.")
     }    
     
