99eac357865ce399b584d2f4e54b398baa5815d0
Arlin <arlinkeo@gmail.com>
2015-11-19 23:43:00 +0100

    getAncestor function for cluster names

--- src/ara/Cluster.scala
+++ src/ara/Cluster.scala
@@ -20,8 +20,27 @@ object Cluster {
       case "L4.2.2.1.1.2.2.1.1.1.1.1" => true
       case _ => false
     }
-    
+  
+    def getAncestor: String = s match {
+      case "L1-L2-L3-L4" => "MTBC"
+      case "L5-L6-LB" => "MTBC"
+      case "L1" => "L1-L2-L3-L4"
+      case "L2-L3-L4" => "L1-L2-L3-L4"
+      case "L2-L3" => "L2-L3-L4"
+      case "L4" => "L2-L3-L4"
+      case "L2" => "L2-L3"
+      case "L3" => "L2-L3"
+      case "L5" => "L5-L6-LB"
+      case "L6-LB" => "L5-L6-LB"
+      case "L6" => "L6-LB"
+      case "LB" => "L6-LB"
+      case x if (x.contains(".")) => x.dropRight(2)
+      case _ => "not known"
+    }
   }
+  
+  
+  
   
   implicit def seqtoBool(s: String) = new Cluster(s)
 }
