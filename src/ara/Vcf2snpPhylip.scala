cd56a9dcba26be04c8c89e72e2edaac54dc34431
Arlin <arlinkeo@gmail.com>
2015-12-19 15:11:03 +0100

    clean code, use scopt

--- src/ara/Vcf2snpPhylip.scala
+++ src/ara/Vcf2snpPhylip.scala
@@ -5,17 +5,25 @@ import com.sun.org.apache.xalan.internal.xsltc.compiler.Sort
 import java.io.PrintWriter
 import java.io.File
 import ara.Mutation._
+import atk.util.Tool
 
 /**
  * This tool reads VCFs from the path file, and returns a SNP phy-file.
  * It does not remove samples with duplicate names.
  */
-object Vcf2snpPhylip {
+object Vcf2snpPhylip extends Tool {
   val usage = "java -jar ara.jar vcf2snp-phylip [pathfile] [output.phy]"
+
+  case class Config(val vcfPathFile: File = null, val output: String = null)
 
   def main(args: Array[String]) {
 
-    /** Elapsed time function */
+    val parser = new scopt.OptionParser[Config]("java -jar ara.jar markers-qc") {
+      opt[File]('i', "input") required () action { (x, c) => c.copy(vcfPathFile = x) } text ("VCF path file")
+      opt[File]('o', "output") required () action { (x, c) => c.copy(output = x + ".phy") } text ("Output name.")
+    }
+
+    /* Elapsed time function */
     def time[R](block: => R): R = {
       val t0 = System.currentTimeMillis()
       val result = block // call-by-name
@@ -24,8 +32,7 @@ object Vcf2snpPhylip {
       result
     }
 
-
-    /** Get all SNP positions */
+    /* Get all SNP positions */
     def getPositions(fList: List[File]): Map[Int, String] = {
       def getPos(file: File): List[(Int, String)] = {
         val sample = file.getParentFile
@@ -38,25 +45,24 @@ object Vcf2snpPhylip {
       fList.flatMap(getPos(_)).toMap
     }
 
-    /** Function to write in new text file. */
-    def printToFile(f: File)(op: PrintWriter => Unit) {
-      val writer = new PrintWriter(f)
-      try { op(writer) } finally { writer.close() }
-    }
-
-    /** Return new name of samples with 9 characters and a space. */
+    /* Return new name of samples with 9 characters and a space. */
     def truncateName(s: String): String = {
       if (s.length > 10) s.substring(s.length - 9, s.length) + " " //Cut s to length 10
       else { val res = "          ".substring(0, 10 - s.length); s + res } //Add spaces until length 10
     }
 
-    /** List all VCFs from the path file, get all SNP positions, and print SNP sequence of each sample. */
-    args.length match {
-      case 2 => time {
-        val fileList = Source.fromFile(new File(args(0))).getLines.map(new File(_)).toList
+    /**
+     *  List all VCFs from the path file, get all SNP positions,
+     *  and print SNP sequence of each sample.
+     */
+
+    parser.parse(args, Config()) map { config =>
+      time {
+        val fileList = tLines(config.vcfPathFile).map(new File(_)).toList
         val refMap = getPositions(fileList) // Map with ref. positions and bases
         println("Writing phy-file...")
-        printToFile(new File(args(1))) { p =>
+        val p = new PrintWriter(config.output)
+        
           p.println(fileList.size + 1 + " " + refMap.size) //Print total number of sequences (VCF's) + reference (1st sequence) and total number of SNP positions.
           p.print("H37RV_V5  ")
           refMap.keysIterator.toList.sorted.foreach(pos => p.print(refMap(pos)))
@@ -73,15 +79,15 @@ object Vcf2snpPhylip {
               else refMap(pos)).mkString
             p.println
             p.print(truncateName(name) + snpSeq)
-            println(name + ":\t" + snpMap.size + "\tSNPs")
-          }
+            println(name + ":\t" + snpMap.size + "\tSNPs")          
         }
+        p.close
         println("Total of " + fileList.size + " VCFs read.")
         println("Length of SNP sequences: " + refMap.size)
         println("Output: " + args(1))
       }
-      case _ => println(usage)
     }
+
   }
 
 }
