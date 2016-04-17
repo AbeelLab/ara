c6a3e2c5e5e3d6a381df57f879119aeba2d4d0ba
dkeo <dkeo@2-lkeb-16-ake1.lumcnet.prod.intern>
2016-04-17 21:21:20 +0200

    print cov

--- src/ara/ListAraResults.scala
+++ src/ara/ListAraResults.scala
@@ -19,7 +19,7 @@ object ListAraResults {
       val pw = new PrintWriter(new File(config.out))
       pw.println("# Ara results")
       pw.println("# Command: " + args.mkString(" "))
-      
+      pw.println("#Study\tsampleID\tPredicted lineage(s) (frequency, read depth)")
       
       study.foreach{study => 
         println("Reading " + study.getName + "...")
@@ -27,8 +27,25 @@ object ListAraResults {
         sampleDirs.foreach{ s =>
           val files = s.listFiles().filter(f => f.getName.endsWith(".interpret.ara"))
           if (!files.isEmpty) {
-            val line = Source.fromFile(files.head).getLines().toList(5)
-            pw.println(study.getName + "\t" + s.getName() + "\t" + line.drop(20))
+            val lines = Source.fromFile(files.head).getLines().toList
+            val predicted = lines(5).drop(20)
+            
+            if (predicted == "NONE"){
+              pw.println(study.getName + "\t" + s.getName() + "\t" + predicted)
+            } else {
+              val lineages = predicted.split(", ").map(_.split("\\(").head)
+              val covPerLin = lines.drop(8).grouped(4).map{linInfo =>
+                (linInfo(0).replaceAll("\\s", "").split("->").last -> linInfo(1).replaceAll("\\s", "").split(":").last)
+              }.toMap
+              
+              //println(predicted)
+              val freqPerLin = predicted.split(", ").map{l => val arr = l.split("\\("); (arr(0) -> arr(1).dropRight(1))}.toMap
+              //println(freqPerLin)
+              
+              val predicted2 = lineages.map(lin => lin + "(" + freqPerLin(lin) + "," + covPerLin(lin) + ")").mkString(", ")
+              pw.println(study.getName + "\t" + s.getName() + "\t" + predicted2)
+            }
+            
           }
         }
       }
